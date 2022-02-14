1ebfd64fa924851c35e3e9d42d398eda385fdb0c
gonzalogonzalo <calleriza.gonzalo@gmail.com>
2022-02-14 12:26:33 +0100

    Finished the static search, need to work on filter

---
+++ components/SearchFilters.jsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react';
+import { Flex, Select, Box, Text, Input, Spinner, Icon, Button } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import { MdCancel } from 'react-icons/md-button';
+import Image from 'next/images/image';
+
+const SearchFilters = () => {
+
+
+    return(
+
+    )
+}
+
+export default SearchFilters;
+
+
+  
+  
